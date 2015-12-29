3621bb53ff01d68fd27e3ab82c0e9754f049668c
Emily MaRee <lee@mleemaree.com>
2015-12-28 22:30:43 -0600

    Iteration two of app exercise

--- ruby/app/app.rb
+++ ruby/app/app.rb
@@ -7,8 +7,21 @@ class App
 	end
 
 	def evaluation
-		puts "#{@author} has developed #{@title} for #{@platform} and he has got #{@rating} points"
+		if @rating.between?(0,4)
+			rating = "poor"
+		elsif @rating.between?(5,6) 
+			rating = "good"
+		elsif @rating.between?(7,9)
+			rating = "very Cool"
+		elsif @rating == 10
+			rating = "awesome"
+		else
+			puts "unrated"
+		end
+
+		puts "#{@author} has developed #{@title} for #{@platform} and he has made a #{rating} application"
 	end
+
 end
 
 app = App.new "ILoveApples", "Richard Stallman", "iOS", 8 
